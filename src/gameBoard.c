8b1b58e90f5ca13c4c279f7b3f4bfaea0c5f3567
sajan10 <sajankaur10@gmail.com>
2016-08-11 23:00:04 -0500

    Pause
    - The function will pause once user hit the  pause bottom

--- src/gameBoard.c
+++ src/gameBoard.c
@@ -1,6 +1,8 @@
 //checks if pause state is 1 or 0. 0 is run and 1 is pause. if pause do a spinlock
 void pauseGame(GameBoard game)
 {
-
+ if (game.pause_state == 0)
+     run_state;
+    while(pause_state== 1)
 
 }
