7a85612fd4d2d3abbb2ec34fcf15edaefdb1410b
princepratik32 <prince.pratik32@gmail.com>
2016-08-17 18:53:38 +0000

    Added Monster.c changes from branch integration_changes

--- src/Monster.c
+++ src/Monster.c
@@ -1,15 +1,59 @@
 #include "../header/monster.h"
+#include "../header/gameBoard.h"
 #include <ncurses.h>
+#include "../header/pacman.h"
+#include <unistd.h>
+#include <time.h>
+#include <stdlib.h>
 
-void moveMonster(Monster *mon)
+#define MAGIC 5
+#define OUTERWALL 'X'
+void randomDirection(int *, int *);
+
+void *moveMonster(void *t)
 {
-    mon[0].y_position = mon[0].y_position-1;
-    mon[0].x_position = mon[0].x_position;
+	threadData* t1 = (threadData *)t;
+	PacMan *p1 = t1->pacPointer;
+	Monster *mon = t1->monPointer;
+	GameBoard *gb = t1->gbPointer;
+
+	srand(time(NULL));
+	int x_next, y_next;
+	int magicNum = MAGIC;
+	for(int i = 0; i < gb->numMonster; ++i)
+	{
+		randomDirection(&(mon[i].x_direction), &(mon[i].y_direction));
+	}
+	while(0 != p1->quit)
+	{
+		for(int i = 0; i < gb->numMonster; ++i)
+		{
+			y_next = mon[i].y_position + mon[i].y_direction;
+			x_next = mon[i].x_position + mon[i].x_direction;
+			if((gb->map[y_next][x_next] == gb->wall) || (y_next == -1) || (y_next == 18) || (x_next == -1) || (x_next == 26))
+			{
+				randomDirection(&(mon[i].x_direction), &(mon[i].y_direction));
+				continue;
+			}
+			if(magicNum == 0)
+			{
+				randomDirection(&(mon[i].x_direction), &(mon[i].y_direction));
+				magicNum = MAGIC;
+				continue;
+			}
+			--magicNum;
+			mon[i].y_position = y_next;
+			mon[i].x_position = x_next;
+		}
+		usleep(250000);
+	}
 }
 
-void displayMonsters(Monster mon, int numMonsters, WINDOW *gameArea)
+void displayMonsters(Monster *mon, int numMonsters, WINDOW *gameArea)
 {
-    mvwprintw(gameArea, mon.y_position+1, mon.x_position+1, "%c", mon.sprite);
+	for(int i = 0; i < numMonsters; ++i){
+		mvwprintw(gameArea, mon[i].y_position+1, mon[i].x_position+1, "%c", mon[i].sprite);
+	}
 }
 
 void monsterRespawn(Monster *mon, int numMonsters)
@@ -25,35 +69,27 @@ void monsterRespawn(Monster *mon, int numMonsters)
         }
     }
 }
-void initilizeMonsters(Monster mon[], char a[][26], int numMonsters)
+void randomDirection(int *x, int *y)
 {
-    int i;
-    int j;
-    int k;
-    for(i=0; i < numMonsters; i++)
-    {
-        mon[i].start_positionX = -1;
-        mon[i].x_direction = 0;
-        mon[i].y_direction = 0;
-        mon[i].state = 0;
-        mon[i].alive = 1;
-        mon[i].sprite = 'M';
-        for (j= 0; j < 20; j ++)
-        {
-            for(k = 0; k < 26; k++)
-            {
-                if(a[j][k] == 'M')
-                {
-                    mon[i].start_positionX = k;
-                    mon[i].start_positionY = j;
-                    mon[i].x_position = mon[i].start_positionX;
-                    mon[i].y_position = mon[i].start_positionY;
-                    a[j][k] = 0;
-                    break;
-                }
-            }
-            if(mon[i].start_positionX > 0)
-            break;
-        }
-    }
+	int r;
+	r = (rand()%4) + 1;
+	switch(r)
+	{
+		case 1:
+			*x = 0;
+			*y = -1;
+			break;
+		case 2:
+			*x = 0;
+			*y = 1;
+			break;
+		case 3:
+			*x = -1;
+			*y = 0;
+			break;
+		case 4:
+			*x = 1;
+			*y = 0;
+			break;
+	}
 }
