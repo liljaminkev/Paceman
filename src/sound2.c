06dab82fbe68cbece7102dab5a22040a19653408
sajan10 <sajankaur10@gmail.com>
2016-08-19 14:44:26 -0500

    update this function cant be use i will upload new one
    so this function will work but not in unix

--- src/sound2.c
+++ src/sound2.c
@@ -1,12 +1,12 @@
-include<dos.h>
+//include<dos.h> this won't work in UNIX orry guys im working on it.
 
 main()
 {
     int z;
     for (z=300; z<= 1000; z=z + 20 )
 {
-    sound(z);// use in c dictionary
-    delay(20);// use in c dictionrary
+    sound(z);
+    delay(20);
 
 }
 nosound(); //used in c Dictonary
