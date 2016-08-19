9e527076f6f56d2e6b6426609740eb70310a7949
Kevin <kevchan85@yahoo.com>
2016-08-18 20:39:15 -0700

    Revert "Merge pull request #7 from liljaminkev/eatfruits"
    This reverts commit 2c88b03503368ba9e2c61528c80f846f22af9a6b, reversing
    changes made to fd4f365ed05bacb6a008fa770b596dbe00636fcb.

--- src/singlePlayerGameController.c
+++ src/singlePlayerGameController.c
@@ -5,6 +5,8 @@
 #include "../header/monster.h"
 #include <string.h>
 #include <ncurses.h>
+#include <unistd.h>
+#include <pthread.h>
 #define DELAY2 50000
 
 int max_y = 20;
@@ -37,6 +39,7 @@ int singlePlayerGameController()
     PacMan player1;
     char fullPath[100];
     int level = 1;
+   
 
     pacmanInitialize(&player1); //set pacman initial state;
 
@@ -102,13 +105,14 @@ int singlePlayerGameController()
 char startGame(PacMan *p1, Monster mon[], GameBoard *gb, Fruit f[][26], WINDOW *game, WINDOW *score)
 {
     char k = 0;
+            nodelay(stdscr, FALSE);
             curs_set(FALSE);
             draw_borders(game);
             draw_borders(score);
             displayBoard(gb, game);
             displayFruit(f, game);
             displayPacman(p1, game);
-            displayMonsters(mon[0], gb->numMonster, game);
+	    displayMonsters(mon, gb->numMonster, game);
             wrefresh(game);
             wrefresh(score);
             k = wgetch(game);
@@ -137,6 +141,10 @@ int singlePlayerGameEngine(PacMan *p1, char *fileName)
     char keypress;              //userinput from keyboard
     int winx, winy;
     Fruit f[20][26];
+    
+    pthread_t monsterThread;
+    int monsterThreadRet;
+    threadData threadData;
 
     //window stuff
     WINDOW *gameArea = newwin(20, 28, 0, 0);
@@ -152,20 +160,27 @@ int singlePlayerGameEngine(PacMan *p1, char *fileName)
     pacmanSetInitialPoint(p1, gb.map);
 
     Monster mon[gb.numMonster];
-    initilizeMonsters(mon, gb.map, gb.numMonster);
+    initializeMonsters(mon, gb.map, gb.numMonster);
+    
+    threadData.pacPointer = p1;
+    threadData.gbPointer = &gb;
+    threadData.monPointer = mon;
 
+do{
     keypress = startGame(p1, mon, &gb, f, gameArea, score); //display gameboard and wait for input
 
-    nodelay(gameArea, TRUE);        //once input recived turn off delay from keyboard
 
+    nodelay(gameArea, TRUE);        //once input recived turn off delay from keyboard
+    monsterThreadRet = pthread_create(&monsterThread, NULL, moveMonster, (void*) &threadData);
     do{
         movePacman(p1, gb.wall, gb.map);
-        eatFruit(p1->x_position,p1->y_position, p1,&gb, f);
+        eatFruit(p1->x_position,p1->y_position, p1->score, gb.numFruit1, f);
         draw_borders(gameArea);
         draw_borders(score);
         displayBoard(&gb, gameArea);
         displayFruit(f, gameArea);
         displayPacman(p1, gameArea);
+	displayMonsters(mon, gb.numMonster, gameArea);
         wnoutrefresh(gameArea);
         wnoutrefresh(score);
         doupdate();
@@ -180,6 +195,8 @@ int singlePlayerGameEngine(PacMan *p1, char *fileName)
 
     }while(0 != p1->quit && gb.numFruit1 > 0);
 
+    pthread_cancel(monsterThread);
+}while(0 != p1->quit && gb.numFruit1 > 0);
     delwin(gameArea);
     delwin(score);
     return 1;
