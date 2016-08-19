502f681053ea9c294f50eb57ae0e60d91f174c61
sajan10 <sajankaur10@gmail.com>
2016-08-19 14:47:06 -0500

    updated pointer
    pointer was missing now it will work perfectly :)

--- src/options.c
+++ src/options.c
@@ -1,11 +1,11 @@
 #include <conio.h>
-//#include <iostream>
+//#include <iostream> not needed
 #include<stdio.h>
 #include <fcntl.h>
 
-void ClearHighScore( )
+void ClearHighScore( )//should work now/
 {
-FILE *fd;
+FILE *fd; // pointer
 fd = fopen("highscore.txt", "w+");
 fprintf(fd,"0");
 fclose(fd);
